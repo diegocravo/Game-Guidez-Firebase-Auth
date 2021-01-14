b72e9cc68ad02f5d312344a4236bbf1e2622097f
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-01-14 04:18:17 -0300

    updates

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,19 @@
+// get data
+db.collection("guides")
+  .get()
+  .then((snapshot) => {
+    setupGuides(snapshot.docs);
+  });
+
+// listen for auth status changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("user logged in: ", user);
+  } else {
+    console.log("user logged out");
+  }
+});
+
 // signup
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
@@ -13,13 +29,28 @@ signupForm.addEventListener("submit", (e) => {
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
+});
 
-  // logout
-  const logout = document.querySelector("#logout");
-  logout.addEventListener("click", (e) => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-      console.log("User sign out");
-    });
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+//login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    //close the login modal and reset form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
