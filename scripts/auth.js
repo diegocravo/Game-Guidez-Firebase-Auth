e8492791d7f4eb23ba25509b49f679f4a97c8294
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-01-16 04:47:48 -0300

    updates

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,16 +1,36 @@
 // listen for auth status changes
 auth.onAuthStateChanged((user) => {
   if (user) {
-    db.collection("guides")
-      .get()
-      .then((snapshot) => {
-        setupGuides(snapshot.docs);
-        setupUI(user);
-      });
+    db.collection("guides").onSnapshot((snapshot) => {
+      setupGuides(snapshot.docs);
+      setupUI(user);
+    });
   } else {
     setupUI();
     setupGuides([]);
   }
+});
+
+//create new guides
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  db.collection("guides")
+    .add({
+      title: createForm.title.value,
+      content: createForm["content"].value,
+    })
+    .then(() => {
+      //close modal and reset form
+      console.log("postado");
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
 
 // signup
