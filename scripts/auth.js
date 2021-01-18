6d058fa1cf2c14788d1c3e9ee03e9fb46979d811
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-01-18 06:08:34 -0300

    updates

--- scripts/auth.js
+++ scripts/auth.js
@@ -70,6 +70,10 @@ signupForm.addEventListener("submit", (e) => {
       const modal = document.querySelector("#modal-signup");
       M.Modal.getInstance(modal).close();
       signupForm.reset();
+      signupForm.querySelector(".error").innerHTML = "";
+    })
+    .catch((err) => {
+      signupForm.querySelector(".error").innerHTML = err.message;
     });
 });
 
@@ -89,10 +93,16 @@ loginForm.addEventListener("submit", (e) => {
   const email = loginForm["login-email"].value;
   const password = loginForm["login-password"].value;
 
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    //close the login modal and reset form
-    const modal = document.querySelector("#modal-login");
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((cred) => {
+      //close the login modal and reset form
+      const modal = document.querySelector("#modal-login");
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+      loginForm.querySelector(".error").innerHTML = "";
+    })
+    .catch((err) => {
+      loginForm.querySelector(".error").innerHTML = err.message;
+    });
 });
